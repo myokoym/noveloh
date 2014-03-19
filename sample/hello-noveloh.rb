d92f70defc98ddafe6764a0e4350106d8f89a898
Masafumi Yokoyama <myokoym@gmail.com>
2014-03-20 00:30:11 +0900

    Add suffix "-ja" to the sample scenario file name
    TODO: translate to English.

--- sample/hello-noveloh.rb
+++ sample/hello-noveloh.rb
@@ -3,7 +3,7 @@
 require "noveloh"
 require "yaml"
 
-pages = YAML.load(File.read("scenario.yaml"))
+pages = YAML.load(File.read("scenario-ja.yaml"))
 
 window = Noveloh::Window.new(pages)
 window.show
