5b7cc7d1406684d344901dd5a73e3f009156b6f4
Masafumi Yokoyama <myokoym@gmail.com>
2014-03-21 15:31:29 +0900

    Add tests for Window#draw

--- test/test-window.rb
+++ test/test-window.rb
@@ -6,11 +6,48 @@ class WindowTest < Test::Unit::TestCase
     @window = Noveloh::Window.new(pages)
   end
 
+  private
+  def fixtures_dir
+    File.join(File.expand_path(File.dirname(__FILE__)), "fixtures")
+  end
+
   class DrawTest < self
     class TextTest < self
       def setup
         pages = [
           {"text" => "Hello"},
+        ]
+        @window = Noveloh::Window.new(pages)
+      end
+
+      def test_nothing_raised
+        assert_nothing_raised do
+          @window.draw
+        end
+      end
+    end
+
+    class BackgroundImageTest < self
+      def setup
+        pages = [
+          {"text" => "Hello"},
+          {"background_image" => File.join(fixtures_dir, "leaf.jpg")},
+        ]
+        @window = Noveloh::Window.new(pages)
+      end
+
+      def test_nothing_raised
+        assert_nothing_raised do
+          @window.draw
+        end
+      end
+    end
+
+    class ColorTest < self
+      def setup
+        pages = [
+          {"text" => "Hello"},
+          {"color" => 0xffffffff},
         ]
         @window = Noveloh::Window.new(pages)
       end
