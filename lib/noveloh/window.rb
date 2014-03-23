ef66e6e6753a3136736c6284827cf4ecfa65d651
Masafumi Yokoyama <myokoym@gmail.com>
2014-03-23 14:18:30 +0900

    Extract methods from jump method

--- lib/noveloh/window.rb
+++ lib/noveloh/window.rb
@@ -80,40 +80,52 @@ module Noveloh
       @flags[flag] = false
     end
 
-    # TODO: refactoring
     def jump
-      return unless @pages[@page_index]
-      tags = @pages[@page_index - 1]["jump"]
+      previous_page = @pages[@page_index - 1]
+      return unless previous_page
+      next_page = one_way(previous_page["jump"])  ||
+                  select(previous_page["select"]) ||
+                  flag_check(previous_page["flag_jump"])
+      @page_index = next_page if next_page
+    end
+
+    def one_way(dest_tag)
+      return unless dest_tag
+      @tag_index[dest_tag]
+    end
+
+    def select(tags)
       return unless tags
-      if tags.is_a?(Array)
-        if @cursor.position <= 0
-          @page_index -= 1
-          return
-        end
+
+      if @cursor.position <= 0
+        @page_index -= 1
+        return
       end
 
-      selected_tag = nil
-      if tags.is_a?(Array)
-        selected_tag = tags[@cursor.position - 1]
-      elsif tags.is_a?(Hash)
-        selected_tag = nil
-        tags.each_entry do |key, value|
-          if @flags[key]
-            selected_tag = value
-            break
-          end
-        end
-        selected_tag = tags["else"] unless selected_tag
-      elsif tags.is_a?(String)
-        selected_tag = tags
-      end
+      selected_tag = tags[@cursor.position - 1]
       next_page = @tag_index[selected_tag]
 
-      if next_page
-        @page_index = next_page
-      else
+      unless next_page
         @page_index -= 1
+        return
       end
+
+      next_page
+    end
+
+    def flag_check(candidates)
+      return unless candidates
+
+      selected_tag = nil
+      candidates.each_entry do |key, value|
+        if @flags[key]
+          selected_tag = value
+          break
+        end
+      end
+      selected_tag = candidates["else"] unless selected_tag
+
+      @tag_index[selected_tag]
     end
   end
 end
