7c4e49bded760e95299031fd16e62b37942c14d6
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-04-18 00:25:46 +0900

    Support font size change

--- lib/noveloh/text.rb
+++ lib/noveloh/text.rb
@@ -3,17 +3,19 @@ require "gosu"
 module Noveloh
   class Text
     def initialize(window, font_size)
+      @window = window
       @font_size = font_size
-      @font = Gosu::Font.new(window, Gosu.default_font_name, @font_size)
+      @default_font = create_font
       @text = nil
       @color = nil
     end
 
     def draw
       return unless @text
+      font = @custom_font || @default_font
       @text.each_line.with_index do |line, i|
         line.chomp!
-        @font.draw(line,
+        font.draw(line,
                    10, 10 + @font_size * i,
                    10,
                    1.0, 1.0,
@@ -23,12 +25,25 @@ module Noveloh
 
     def apply_page(page)
       @text = nil
+      @custom_font = nil
       return unless page
       text = page["text"]
       return unless text
       text.encode!("UTF-8")
       @text = text
       @color = page["color"] || 0xffffffff
+      if page["font_size"]
+        @custom_font = create_font({
+          size: page["font_size"],
+        })
+      end
+    end
+
+    private
+    def create_font(options={})
+      name = Gosu.default_font_name
+      size = options[:size] || @font_size
+      Gosu::Font.new(@window, name, size)
     end
   end
 end
